05069439460f4a85be765554e4526ae9a3ae14ee
Sandeep007-Stack <sandeep.bansod.007@gmail.com>
2025-06-16 11:34:45 +0530

    Update

--- script.js
+++ script.js
@@ -17,15 +17,19 @@ function calculateNameNumber(name) {
         i: 1, k: 2, g: 3, m: 4, h: 5, v: 6, z: 7, p: 8,
         j: 1, r: 2, l: 3, t: 4, n: 5, w: 6,
         q: 1,       s: 3,       x: 5,
-        y: 1
+        y: 1,
+        '0': 0, '1': 1, '2': 2, '3': 3, '4': 4,
+        '5': 5, '6': 6, '7': 7, '8': 8, '9': 9
     };
 
     let sum = 0;
     name.toLowerCase().replace(/\s+/g, '').split('').forEach(char => {
-        sum += letterValues[char] || 0;
+        if (char in letterValues) {
+            sum += letterValues[char];
+        }
     });
 
-    while (sum > 9) {
+    while (sum > 9 && sum !== 11 && sum !== 22 && sum !== 33) {
         sum = sum.toString().split('').reduce((acc, val) => acc + parseInt(val), 0);
     }
 
